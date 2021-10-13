24803df32cac8e651b9c0eeb61d520aa903e156f
ilia-web <60225314+ilia-web@users.noreply.github.com>
2021-10-13 15:48:10 +0400

    .

---
+++ lesson2_task2.java
@@ -0,0 +1,15 @@
+public class lesson2_task2 {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 50; i++){
+            if (i%2 == 0){
+                System.out.println(i);
+            }
+        }
+
+        for (int i = 50; i <= 100; i++){
+            if (i%2 != 0){
+                System.out.println(i);
+            }
+        }
+    }
+}
